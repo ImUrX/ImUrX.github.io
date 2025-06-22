d1f346ade1694af9a06a1344eb27d28db98a57e7
Uriel <imurx@proton.me>
2025-06-22 04:25:12 -0400

    add google fonts as link instead of CSS import

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,22 @@
 import { Route, Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import "./app.css";
-import { MetaProvider } from "@solidjs/meta";
-import { ParentComponent, Suspense } from "solid-js";
+import { Link, MetaProvider } from "@solidjs/meta";
+import { Suspense } from "solid-js";
 
 const filter = {
   lang: ["ja", "es"],
 };
 
-
 export default function App() {
   return (
     <MetaProvider>
+      <Link rel="preconnect" href="https://fonts.googleapis.com" />
+      <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+      <Link
+        href="https://fonts.googleapis.com/css2?family=Noto+Color+Emoji&family=Noto+Emoji:wght@300..700&family=Noto+Sans+JP:wght@100..900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet"
+      />
       <Router
         root={(props) => (
           <>
@@ -19,10 +24,7 @@ export default function App() {
           </>
         )}
       >
-        <Route
-          path="/:lang?/"
-          matchFilters={filter}
-        >
+        <Route path="/:lang?/" matchFilters={filter}>
           <FileRoutes />
         </Route>
       </Router>
