04c046170660172f396764e4e340f47503552b88
Uriel <imurx@proton.me>
2026-02-26 17:33:30 -0300

    add more checks for last.fm

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -33,7 +33,9 @@ const recentTracksQuery = query(async () => {
   const json = await response?.json().catch(() => null);
   if (
     !response?.ok ||
-    json?.recenttracks?.track?.[0]?.["@attr"]?.nowplaying !== "true"
+    json?.recenttracks?.track?.[0]?.["@attr"]?.nowplaying !== "true" ||
+    !Array.isArray(json.recenttracks.image) ||
+    json.recenttracks.image.length === 0
   ) {
     return 0;
   }
